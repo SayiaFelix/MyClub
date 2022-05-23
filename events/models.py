c7365a45538b889d709e71babc5b96a5de736bf1
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-23 14:52:46 +0300

    Styling admin section

--- events/models.py
+++ events/models.py
@@ -1,15 +1,16 @@
 from calendar import c
 from pydoc import describe
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Venue(models.Model):
     name = models.CharField("Venue Name",max_length=120)
     address = models.CharField("Address",max_length=300)
-    zip_code = models.CharField("Zip Code",max_length=15)
-    phone = models.CharField("Contact Number",max_length=30)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField("Email Address")
+    zip_code = models.CharField("Zip Code",max_length=15,blank=True)
+    phone = models.CharField("Contact Number",max_length=30,blank=True)
+    web = models.URLField('Website Address',blank=True)
+    email_address = models.EmailField("Email Address",blank=True)
 
 
     def __str__(self):
@@ -29,7 +30,7 @@ class Event(models.Model):
     name = models.CharField('Events Name',max_length = 120)
     event_date = models.DateTimeField('Events Date') 
     venue = models.ForeignKey(Venue,on_delete=models.CASCADE,blank=True,null=True)
-    manager = models.CharField('Events Manager',max_length =60)
+    manager = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
     description= models.TextField(blank=True)
     attendees=models.ManyToManyField(MyClubUser,blank=True)
 
