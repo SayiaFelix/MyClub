577e5c5e3be528b93db2e11c3a61e863dd483ec0
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-24 18:00:35 +0300

    creating article search

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('<int:year>/<str:month>',views.home, name='home'),
     path('events',views.all_events, name='list-events'),
     path('venue',views.add_venue, name='venue'),
+    path('list_venue',views.list_venues, name='list_venue'),
+    path('show_venue/<venue_id>',views.show_venues, name='show_venue'),
 ]
--- events/views.py
+++ events/views.py
@@ -3,11 +3,28 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
-from .models import Event
+from .models import Event,Venue
 from .forms import VenueForm
 
 
 # Create your views here.
+def show_venues(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+
+    return render(request,'events/show.html',{'venue':venue})
+
+
+
+
+
+def list_venues(request): 
+
+    venue_list= Venue.objects.all()
+
+    return render(request,'events/venues.html',{'venue_list':venue_list})
+
+
+
 def add_venue(request):
 
     submitted = False
